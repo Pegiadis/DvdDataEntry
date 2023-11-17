895e7efdff4f0a332451a8563404fe9f258b45d3
Pegiadis <username@users.noreply.github.com>
2023-11-17 12:05:00 +0200

    Add new models

---
+++ DvdDataEntry/Models/CustomerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DvdDataEntry.Models
+{
+    internal class CustomerModel
+    {
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public int AddressId { get; set; }
+        public bool Activebool { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public int Active { get; set; }
+    }
+}
